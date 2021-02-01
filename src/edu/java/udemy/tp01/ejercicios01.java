f8d8b47269719812f431c3d467cfef67758bb9ca
javierniz <javier_aniz@hotmail.com>
2021-02-01 18:53:44 -0300

    Scanner

--- src/edu/java/udemy/tp01/ejercicios01.java
+++ src/edu/java/udemy/tp01/ejercicios01.java
@@ -6,14 +6,19 @@ public class ejercicios01 {
 
 	public static void main(String[] args) {
 				
-		
-		
-		
-		
-		
-	}		
-		
-}		
+		//ejercicio 6 
+		 
+		 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporciona el numero1:");
+        int numero1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Proporciona el numero2:");
+        int numero2 = Integer.parseInt(scanner.nextLine());
+        System.out.println("El numero mayor es:");
+        System.out.println(numero1 > numero2 ? numero1 : numero2);
+        
+    }
+}
 
 
 /*ejercicio 6 
